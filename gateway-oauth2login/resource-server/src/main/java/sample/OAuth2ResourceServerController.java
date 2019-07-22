a1bc67c7082eae8cfae98808e39885d09b496eb8
manishganta <manishteja.ganta@gmail.com>
2019-07-21 22:09:07 -0600

    Add files via upload

---
+++ gateway-oauth2login/resource-server/src/main/java/sample/OAuth2ResourceServerController.java
@@ -0,0 +1,16 @@
+
+package sample;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OAuth2ResourceServerController {
+
+	@GetMapping("/resource")
+	public String resource(@AuthenticationPrincipal Jwt jwt) {
+		return String.format("Resource accessed by %s (sub)", jwt.getSubject());
+	}
+}
